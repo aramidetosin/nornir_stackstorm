1f46eb66b06efaa6cadafafa84caeffad5b7ac57
Tosin <aoluwatosin10@gmail.com>
2020-05-01 00:04:15 +0000

    Update with netbox parameters

---
+++ Nornir/scripts/helpers.py
@@ -0,0 +1,20 @@
+from secrets import creds
+from nornir import InitNornir
+from pprint import pprint
+
+# Tranform Functions
+
+
+def adapt_user_password(host):
+    host.username = creds[f"{host}"]["username"]
+    host.password = creds[f"{host}"]["password"]
+
+
+# Netbox
+
+
+# CLI
+
+if __name__ == "__main__":
+        nr = InitNornir(config_file="../config.yaml")
+        pprint(nr.inventory.get_inventory_dict())
